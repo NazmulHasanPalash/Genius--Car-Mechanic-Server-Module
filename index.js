b06efa42ad0f226d4870e631eb21bfbfe6cfc311
polash <an>
2021-10-26 01:13:05 +0600

    work done

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfvsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
-        const database = client.db('carMechanic');
+        const database = client.db('CarDetails');
         const servicesCollection = database.collection('services');
 
         // GET API
